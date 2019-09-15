83b9cecc8d5ae015faa56ac9bce5bc1749b8b9c3
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-09-15 22:45:18 +0300

    Project initialization. Closes #1

---
+++ src/main/java/com/uvytautas/mqcrosswalk/MqCrosswalkApplication.java
@@ -0,0 +1,13 @@
+package com.uvytautas.mqcrosswalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqCrosswalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqCrosswalkApplication.class, args);
+	}
+
+}

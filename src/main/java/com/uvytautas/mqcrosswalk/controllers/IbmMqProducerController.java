5bb138e5477fb6746a82808bae7c1706bbea191a
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-09-19 22:23:48 +0300

    Added simple Ibm MQ integration. Closes #3

---
+++ src/main/java/com/uvytautas/mqcrosswalk/controllers/IbmMqProducerController.java
@@ -0,0 +1,39 @@
+package com.uvytautas.mqcrosswalk.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ibmMq")
+public class IbmMqProducerController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IbmMqProducerController.class);
+
+    @Value("${ibm.mq.destination.queue}")
+    private String destinationQueue;
+
+    private final JmsTemplate jmsTemplate;
+
+    public IbmMqProducerController(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @PostMapping("send")
+    public void send(@RequestBody String request) {
+        try {
+            jmsTemplate.convertAndSend(destinationQueue, request);
+
+        } catch (JmsException ex) {
+            LOGGER.error("Can't post to Ibm mq", ex);
+
+        }
+    }
+
+}

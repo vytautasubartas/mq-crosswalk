e9f0a21391b2f61750213726a8035f2fdf9a6d3c
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-09-29 22:43:47 +0300

    Added logging processor. Closes: #14

---
+++ src/main/java/com/uvytautas/mqcrosswalk/camel/processors/TraceLogProcessor.java
@@ -0,0 +1,20 @@
+package com.uvytautas.mqcrosswalk.camel.processors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TraceLogProcessor implements Processor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TraceLogProcessor.class);
+
+    @Override
+    public void process(Exchange exchange) {
+        Message message = exchange.getMessage();
+        LOGGER.info("Body: {}\nHeaders: {}", message.getBody(String.class), message.getHeaders());
+    }
+}
--- src/main/java/com/uvytautas/mqcrosswalk/camel/routes/MqCrosswalkRoutes.java
+++ src/main/java/com/uvytautas/mqcrosswalk/camel/routes/MqCrosswalkRoutes.java
@@ -1,18 +1,28 @@
 package com.uvytautas.mqcrosswalk.camel.routes;
 
+import com.uvytautas.mqcrosswalk.camel.processors.TraceLogProcessor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MqCrosswalkRoutes extends RouteBuilder {
 
+
+    private final TraceLogProcessor traceLogProcessor;
+
+    public MqCrosswalkRoutes(TraceLogProcessor traceLogProcessor) {
+        this.traceLogProcessor = traceLogProcessor;
+    }
+
     @Override
     public void configure() {
 
-        from("jetty:http://0.0.0.0:9999/input").to("ibmmq:queue:DEV.QUEUE.1");
+        from("jetty:http://0.0.0.0:9999/input")
+                .process(traceLogProcessor)
+                .to("ibmmq:queue:DEV.QUEUE.1");
 
         from("ibmmq:queue:DEV.QUEUE.1")
-                .log("Passing to activemq")
+                .process(traceLogProcessor)
                 .inOnly("activemq:queue:ibmmqconsumer");
     }
 }

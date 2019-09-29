f666d19a5683be3e9c98d7442532f95b8b3fa69f
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-09-29 23:42:51 +0300

    started storing request bodies in DB. Closes: #19

---
+++ src/main/java/com/uvytautas/mqcrosswalk/domain/Document.java
@@ -0,0 +1,45 @@
+package com.uvytautas.mqcrosswalk.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String documentCode;
+    private String documentBody;
+
+    public Document(String documentCode, String documentBody) {
+        this.documentCode = documentCode;
+        this.documentBody = documentBody;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDocumentCode() {
+        return documentCode;
+    }
+
+    public void setDocumentCode(String documentCode) {
+        this.documentCode = documentCode;
+    }
+
+    public String getDocumentBody() {
+        return documentBody;
+    }
+
+    public void setDocumentBody(String documentBody) {
+        this.documentBody = documentBody;
+    }
+}

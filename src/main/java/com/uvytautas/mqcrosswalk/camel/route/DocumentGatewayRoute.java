e5f43230f13a42c8786d649f79df3b1b9e952df6
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-10-13 00:05:56 +0300

    Moved choice route to separate class. Closes: #41

---
+++ src/main/java/com/uvytautas/mqcrosswalk/camel/route/DocumentGatewayRoute.java
@@ -0,0 +1,21 @@
+package com.uvytautas.mqcrosswalk.camel.route;
+
+import com.uvytautas.mqcrosswalk.camel.util.CommonConstants;
+import com.uvytautas.mqcrosswalk.camel.util.DocumentTypes;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DocumentGatewayRoute extends RouteBuilder {
+    @Override
+    public void configure() {
+        from("direct:documentGateway").choice().when(header(CommonConstants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.MASTER.toString()))
+                .to("direct:master")
+                .endChoice()
+                .when(header(CommonConstants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.UPDATE.toString()))
+                .to("direct:update")
+                .endChoice()
+                .otherwise().log("UNKNOWN DOC TYPE")
+                .end();
+    }
+}

681198f2044080f1c5625bc8db6fc0dd986c405a
vytautasubartas <vytautas.ubartas@zenitech.co.uk>
2019-10-12 22:54:24 +0300

    Created header processor. Closes: #33

---
+++ src/main/java/com/uvytautas/mqcrosswalk/camel/processor/header/PayloadHeaderProcessor.java
@@ -0,0 +1,35 @@
+package com.uvytautas.mqcrosswalk.camel.processor.header;
+
+import com.uvytautas.mqcrosswalk.camel.util.CommonConstants;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
+
+@Component
+public class PayloadHeaderProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        Message message = exchange.getMessage();
+
+        ByteArrayInputStream payloadBody = exchange.getMessage().getBody(ByteArrayInputStream.class);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(payloadBody);
+        String documentCode = document.getDocumentElement().getAttribute("code");
+        String documentType = document.getDocumentElement().getAttribute("type");
+
+        message.setHeader(CommonConstants.DOCUMENT_CODE_HEADER, documentCode);
+        message.setHeader(CommonConstants.DOCUMENT_TYPE_HEADER, documentType);
+    }
+}
--- src/main/java/com/uvytautas/mqcrosswalk/camel/route/IbmMqInputRoute.java
+++ src/main/java/com/uvytautas/mqcrosswalk/camel/route/IbmMqInputRoute.java
@@ -2,8 +2,9 @@ package com.uvytautas.mqcrosswalk.camel.route;
 
 import com.uvytautas.mqcrosswalk.camel.processor.document.MasterDocumentProcessor;
 import com.uvytautas.mqcrosswalk.camel.processor.document.UpdateDocumentProcessor;
+import com.uvytautas.mqcrosswalk.camel.processor.header.PayloadHeaderProcessor;
 import com.uvytautas.mqcrosswalk.camel.processor.logging.TraceLogProcessor;
-import com.uvytautas.mqcrosswalk.camel.util.Constants;
+import com.uvytautas.mqcrosswalk.camel.util.CommonConstants;
 import com.uvytautas.mqcrosswalk.camel.util.DocumentTypes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
@@ -15,12 +16,14 @@ public class IbmMqInputRoute extends RouteBuilder implements UriBased {
     private final TraceLogProcessor traceLogProcessor;
     private final MasterDocumentProcessor masterDocumentProcessor;
     private final UpdateDocumentProcessor updateDocumentProcessor;
+    private final PayloadHeaderProcessor payloadHeaderProcessor;
 
-    public IbmMqInputRoute(TraceLogProcessor traceLogProcessor, MasterDocumentProcessor masterDocumentProcessor, UpdateDocumentProcessor updateDocumentProcessor) {
+    public IbmMqInputRoute(TraceLogProcessor traceLogProcessor, MasterDocumentProcessor masterDocumentProcessor, UpdateDocumentProcessor updateDocumentProcessor, PayloadHeaderProcessor payloadHeaderProcessor) {
         this.traceLogProcessor = traceLogProcessor;
         this.masterDocumentProcessor = masterDocumentProcessor;
         this.updateDocumentProcessor = updateDocumentProcessor;
         this.uri = "ibmmq:queue:DEV.QUEUE.1";
+        this.payloadHeaderProcessor = payloadHeaderProcessor;
     }
 
     public String getUri() {
@@ -32,13 +35,12 @@ public class IbmMqInputRoute extends RouteBuilder implements UriBased {
 
         from(uri)
                 .process(traceLogProcessor)
-                .setHeader(Constants.DOCUMENT_CODE_HEADER).xpath("/Document/@code", String.class)
-                .setHeader(Constants.DOCUMENT_TYPE_HEADER).xpath("/Document/@type", String.class)
-                .choice().when(header(Constants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.MASTER.toString()))
+                .process(payloadHeaderProcessor)
+                .choice().when(header(CommonConstants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.MASTER.toString()))
                 .log("Master received")
                 .process(masterDocumentProcessor)
                 .endChoice()
-                .when(header(Constants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.UPDATE.toString()))
+                .when(header(CommonConstants.DOCUMENT_TYPE_HEADER).isEqualTo(DocumentTypes.UPDATE.toString()))
                 .log("Update received")
                 .process(updateDocumentProcessor)
                 .endChoice()
